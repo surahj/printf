c45e39d6cdcf271c1b4ec3cd2e6d34005995ed95
Odebode Opeyemi <31376475+surahj@users.noreply.github.com>
2022-04-21 20:50:56 +0100

    Update print_x.c

--- print_x.c
+++ print_x.c
@@ -1,38 +1,43 @@
 #include "main.h"
-#include <stdlib.h>
-
 /**
- * print_x - A function that prints an ascii char value in
- * lowercase hexadecimal
- * @x: char to print
- * Return: number of printed characters
+ * print_hex - prints a decimal in hexadecimal
+ * @arguments: input string
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: number of chars printed
  */
-int print_x(va_list x)
+int print_hex(va_list arguments, char *buf, unsigned int ibuf)
 {
-	unsigned int a[8];
-	unsigned int j = 1, m = 268435456, n, sum = 0;
-	char diff;
-	int counter = 0;
+	int int_input, i, isnegative, count, first_digit;
+	char *hexadecimal, *binary;
 
-	n = va_arg(x, unsigned int);
-	diff = 'a' - ':';
-	a[0] = n / m;
-	for (; j < 8; j++)
+	int_input = va_arg(arguments, int);
+	isnegative = 0;
+	if (int_input == 0)
 	{
-		m /= 16;
-		a[j] = (n / m) % 16;
+		ibuf = handl_buf(buf, '0', ibuf);
+		return (1);
 	}
-	for (j = 0; j < 8; j++)
+	if (int_input < 0)
 	{
-		sum += a[j];
-		if (sum != 0 || j == 7)
+		int_input = (int_input * -1) - 1;
+		isnegative = 1;
+	}
+	binary = malloc(sizeof(char) * (32 + 1));
+	binary = fill_binary_array(binary, int_input, isnegative, 32);
+	hexadecimal = malloc(sizeof(char) * (8 + 1));
+	hexadecimal = fill_hex_array(binary, hexadecimal, 0, 8);
+	for (first_digit = i = count = 0; hexadecimal[i]; i++)
+	{
+		if (hexadecimal[i] != '0' && first_digit == 0)
+			first_digit = 1;
+		if (first_digit)
 		{
-			if (a[j] < 10)
-				_putchar('0' + a[j]);
-			else
-				_putchar('0' + diff + a[j]);
-			counter++;
+			ibuf = handl_buf(buf, hexadecimal[i], ibuf);
+			count++;
 		}
 	}
-	return (counter);
+	free(binary);
+	free(hexadecimal);
+	return (count);
 }
