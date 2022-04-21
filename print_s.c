2eb223f0211a68ba39fc155a931f2c16570951b1
Odebode Opeyemi <31376475+surahj@users.noreply.github.com>
2022-04-21 09:59:42 +0100

    Update print_s.c

--- print_s.c
+++ print_s.c
@@ -1,19 +1,26 @@
 #include "main.h"
-#include <unistd.h>
 
 /**
- * print_s - A function that prints a string
- * @s: string to print
- * Return: i
+ * print_str - writes the string to stdout
+ * @arguments: input string
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: On success 1.
  */
-int print_s(va_list s)
+int print_str(va_list arguments, char *buf, unsigned int ibuf)
 {
-	char *str = va_arg(s, char *);
-	int i = 0;
+	char *str;
+	unsigned int i;
+	char nill[] = "(null)";
 
+	str = va_arg(arguments, char *);
 	if (str == NULL)
-		str = "(null)";
-	while (str[i])
-		_putchar(str[i++]);
+	{
+		for (i = 0; nill[i]; i++)
+			ibuf = handl_buf(buf, nill[i], ibuf);
+		return (6);
+	}
+	for (i = 0; str[i]; i++)
+		ibuf = handl_buf(buf, str[i], ibuf);
 	return (i);
 }
