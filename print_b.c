ddd87a45e9ebaa4f5cb0b6d1b5d12b7898b6cc28
Odebode Opeyemi <31376475+surahj@users.noreply.github.com>
2022-04-21 10:04:13 +0100

    Update print_b.c

--- print_b.c
+++ print_b.c
@@ -1,32 +1,42 @@
 #include "main.h"
 
 /**
- * print_b - A function that prints an unsigned int i binary notation
- * @b: unsigned in to print
- * Return: number of printed digits
+ * print_bnr - prints decimal in binary
+ * @arguments: input string
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: number of chars printed.
  */
-int print_b(va_list b)
+int print_bnr(va_list arguments, char *buf, unsigned int ibuf)
 {
-	unsigned int n, m = 2147483648, j = 1, sum = 0;
-	unsigned int a[32];
-	int counter = 0;
+	int int_input, count, i, first_one, isnegative;
+	char *binary;
 
-	n = va_arg(b, unsigned int);
-	a[0] = n / m;
-
-	for (; j < 32; j++)
+	int_input = va_arg(arguments, int);
+	isnegative = 0;
+	if (int_input == 0)
 	{
-		m /= 2;
-		a[j] = (n / m) % 2;
+		ibuf = handl_buf(buf, '0', ibuf);
+		return (1);
 	}
-	for (j = 0; j < 32; j++)
+	if (int_input < 0)
 	{
-		sum += a[j];
-		if (sum || j == 31)
+		int_input = (int_input * -1) - 1;
+		isnegative = 1;
+	}
+	binary = malloc(sizeof(char) * (32 + 1));
+	binary = fill_binary_array(binary, int_input, isnegative, 32);
+	first_one = 0;
+	for (count = i = 0; binary[i]; i++)
+	{
+		if (first_one == 0 && binary[i] == '1')
+			first_one = 1;
+		if (first_one == 1)
 		{
-			_putchar('0' + a[j]);
-			counter++;
+			ibuf = handl_buf(buf, binary[i], ibuf);
+			count++;
 		}
 	}
-	return (counter);
+	free(binary);
+	return (count);
 }
