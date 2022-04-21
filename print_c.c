e9b6bb74fd0ee85b485fe8c1b2587d716a619b06
Odebode Opeyemi <31376475+surahj@users.noreply.github.com>
2022-04-21 09:57:35 +0100

    Update print_c.c

--- print_c.c
+++ print_c.c
@@ -1,14 +1,18 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * print_c - A function that prints a char
- * @c: character to print
- * Return: Always 1 (Success)
+ * print_chr - writes the character c to stdout
+ * @arguments: input char
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: On success 1.
  */
-int print_c(va_list c)
+int print_chr(va_list arguments, char *buf, unsigned int ibuf)
 {
-	char character = (char)va_arg(c, int);
+	char c;
 
-	_putchar(character);
+	c = va_arg(arguments, int);
+	handl_buf(buf, c, ibuf);
+
 	return (1);
 }
